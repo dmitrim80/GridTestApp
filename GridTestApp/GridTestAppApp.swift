3e30e198ab2578478007866005526dd07e58b38f
Dmitri Morozov <114444260+dmitrim80@users.noreply.github.com>
2023-05-16 10:41:44 -0400

    Initial Commit

---
+++ GridTestApp/GridTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridTestAppApp.swift
+//  GridTestApp
+//
+//  Created by Dmitri Morozov on 5/16/23.
+//
+
+import SwiftUI
+
+@main
+struct GridTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
